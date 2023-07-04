e5ee4af311397f4105e9d33dad314435a5be1f93
halilcancengiz <hllcncngz1@gmail.com>
2023-07-04 18:27:20 +0300

    animate deleted

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -7,7 +7,7 @@ const Intro = () => {
 
     return (
 
-        <section className="min-w-screen min-h-screen flex items-center justify-center overflow-hidden flex-col after:content-[''] after:animate-opacity after:rounded-full after:h-20 after:w-20 after:bg-transparent after:absolute after:top-[-80px] after:left-[-80px] after:shadow-border-shadow-blue after:sm:block after:xs:hidden before:sm:block before:xs:hidden before:animate-opacity before:content-[''] before:rounded-full before:h-20 before:w-20 before:bg-transparent before:absolute before:bottom-[-80px] before:right-[-80px] before:shadow-border-shadow-blue" >
+        <section className="min-h-screen h-full flex items-center justify-center overflow-hidden flex-col " >
             <Particle />
             <div className="flex justify-center">
                 <img className="w-1/3 min-w-[280px] drop-shadow-blue-btn " src={codetyping} alt="codetyping" />
