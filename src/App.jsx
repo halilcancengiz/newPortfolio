8631c70dcb856eaf3e49d48cc62e59e6e2fe1267
Halil Can Cengiz <88160209+halilcancengiz@users.noreply.github.com>
2024-02-09 19:36:04 +0300

    updated app.js

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React, { memo } from "react";
 import Routes from "./routes/Routes";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { useEffect } from "react";
 
 const App = () => {
   useEffect(() => {
