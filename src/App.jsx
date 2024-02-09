8f1f9b3da458baacae4b7813fca6eae17c943191
Halil Can Cengiz <88160209+halilcancengiz@users.noreply.github.com>
2024-02-09 19:27:24 +0300

    postMessage example

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,21 @@ import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 const App = () => {
+  function sendSize() {
+    window.parent.postMessage(
+      {
+        type: "resize",
+        width: document.documentElement.scrollWidth,
+        height: document.documentElement.scrollHeight
+      },
+      "http://127.0.0.1:5500/index.html"
+    )
+  }
 
+  // when this window loads or resizes …
+  // call our sendSize function
+  window.addEventListener("load", sendSize)
+  window.addEventListener("resize", sendSize)
   return (
     <div className="App bg-[#1F2125] relative font-poppins">
       <Routes />
